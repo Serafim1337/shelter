70f5de10ac98d626697bcfb3e120c0e64c17b981
Serafim <7620226@gmail.com>
2022-03-08 21:08:48 +0300

    feat: implement adaptive dropdown menu

--- pages/main/main-script.js
+++ pages/main/main-script.js
@@ -1,6 +1,16 @@
 const burgerMenu = document.querySelector(".burger-menu");
+const dropdownMenu = document.querySelector(".dropdown-menu");
+const menuBlock = document.querySelector(".menu-block");
+const inPageLinks = document.querySelectorAll(".in-page-link");
+
 burgerMenu.addEventListener("click", burgerMenuHandler);
+for (let link of inPageLinks) {
+  link.addEventListener("click", burgerMenuHandler);
+}
 
 function burgerMenuHandler() {
   burgerMenu.classList.toggle("cross");
+  dropdownMenu.classList.toggle("active-dropdown");
+  menuBlock.classList.toggle("active-menu-block");
+  document.body.classList.toggle("lock");
 }
